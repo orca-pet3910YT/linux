4a3304f9c4d1dcbbff8a7b334ff2f29d6cc34cf3
orca.pet <orca@hydrogen>
2026-02-28 14:12:21 +0100

    #29

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,4 +1,4 @@
 #define UTS_MACHINE		"x86_64"
-#define LINUX_COMPILE_BY	"root"
-#define LINUX_COMPILE_HOST	"Linux"
+#define LINUX_COMPILE_BY	"orca"
+#define LINUX_COMPILE_HOST	"hydrogen"
 #define LINUX_COMPILER		"gcc (Debian 14.2.0-19) 14.2.0, GNU ld (GNU Binutils for Debian) 2.44"
